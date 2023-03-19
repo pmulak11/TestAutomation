60ee7c7c4b7946389b90e19afbc5aa198959dd14
Piotr Mulak <pmulak@gmail.com>
2023-03-19 17:08:28 +0100

    3rd commit

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -13,4 +13,18 @@ public class UltimateQATest extends PageSetup {
         WebElement buttonSuccess = driver.findElement(By.className("entry-title"));
         Assertions.assertEquals("Button success", buttonSuccess.getText());
     }
+
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameInput = driver.findElement(By.id("et_pb_contact_name_0"));
+        nameInput.sendKeys("Pjoter");
+        WebElement emailInput = driver.findElement(By.id("et_pb_contact_email_0"));
+        emailInput.sendKeys("tester@test.com");
+        Thread.sleep(2000);
+        WebElement emailButton = driver.findElement(By.name("et_builder_submit_button"));
+        emailButton.click();
+        Thread.sleep(2000);
+        WebElement thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
+        Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
+    }
 }
