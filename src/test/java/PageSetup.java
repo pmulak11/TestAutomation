9de0a710f05bb958ec621e3b402708b97b320b5b
Piotr <pmulak@gmail.com>
2023-03-20 13:03:57 +0100

    new tests added

--- src/test/java/PageSetup.java
+++ src/test/java/PageSetup.java
@@ -21,6 +21,6 @@ public class PageSetup {
 
     @AfterEach
     public void teardown() {
-        // driver.close();
+        //driver.close();
     }
 }
--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -2,6 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class UltimateQATest extends PageSetup {
 
@@ -19,10 +24,10 @@ public class UltimateQATest extends PageSetup {
         nameInput.sendKeys("Pjoter");
         WebElement emailInput = driver.findElement(By.id("et_pb_contact_email_0"));
         emailInput.sendKeys("tester@test.com");
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         WebElement emailButton = driver.findElement(By.name("et_builder_submit_button"));
         emailButton.click();
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         WebElement thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
         Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
     }
@@ -37,6 +42,21 @@ public class UltimateQATest extends PageSetup {
         Assertions.assertFalse(maleRadioButton.isSelected());
         Assertions.assertFalse(femaleRadioButton.isSelected());
         Assertions.assertTrue(otherRadioButton.isSelected());
+    }
+    @Test
+    public void testCheckBox() {
+        WebElement bikeCheckBox = driver.findElement(By.xpath("//*[@id='post-909']/div/div[1]/div/div[3]/div/div[1]/div[8]/div/div/div/form/input[1]"));
+        bikeCheckBox.click();
+        WebElement carCheckBox = driver.findElement(By.xpath("//*[@id='post-909']/div/div[1]/div/div[3]/div/div[1]/div[8]/div/div/div/form/input[2]"));
+        carCheckBox.click();
+        Assertions.assertTrue(bikeCheckBox.isSelected());
+        Assertions.assertTrue(carCheckBox.isSelected());
+    }
+    @Test
+    public void testDropDown() {
+        Select carDropDown = new Select(driver.findElement(By.xpath("//*[@id='post-909']/div/div[1]/div/div[3]/div/div[1]/div[9]/div/div/div/select")));
+        carDropDown.selectByVisibleText("Saab");
+        Assertions.assertEquals("Saab", carDropDown.getFirstSelectedOption().getText());
     }
 
 }
